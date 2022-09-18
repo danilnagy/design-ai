454d904f2c770b435bea72d96812d1e7682f1241
qh2195 <qh2195@columbia.edu>
2022-09-18 00:47:09 -0400

    Qing Hou Submission
    Qing Hou_qh2195_ Challenge 1 Python script : when distance from attractor to point is smaller than 3, the radius of the circle centered on the point is 0.2. When the distance from attractor to point is equal larger than 3, the radius of the circle centered on the point is 0.6.

---
+++ docs/setup/data/challenge 1.py
@@ -0,0 +1,20 @@
+﻿import Rhino.Geometry as rh
+
+points = []
+circles = []
+
+for x in range(x_num):
+    for y in range(y_num):
+        point = rh.Point3d(x, y, 0.0)
+        points.append(point)
+
+        dist = point.DistanceTo(attractor)
+        if dist < 3:
+            radius= 0.2
+        else:
+            radius=0.6
+        circle = rh.Circle(point, radius)
+        circles.append(circle)
+
+
+
