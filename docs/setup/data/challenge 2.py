5d36f8406b4a45d9acb1b9c733259dd6f167b374
qh2195 <qh2195@columbia.edu>
2022-09-18 00:51:01 -0400

    Create challenge 2.py
    Qing Hou_qh2195_ Challenge 2 Python script : the positions of  all circles, are defined by the size of the spacing.

---
+++ docs/setup/data/challenge 2.py
@@ -0,0 +1,17 @@
+﻿import Rhino.Geometry as rh
+
+points = []
+circles = []
+
+for x in range(x_num):
+    for y in range(y_num):
+        point = rh.Point3d( spacing*x, spacing*y, 0.0)
+        points.append(point)
+
+        dist = point.DistanceTo(attractor)
+        radius= dist / 5
+        circle = rh.Circle(point, radius)
+        circles.append(circle)
+
+
+
