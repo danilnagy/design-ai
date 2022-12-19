b85698bb8e46df5312de560d85a6ce082807ac13
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-12-18 17:12:53 -0800

    zz2984/85

--- docs/projects/packing/packing.py
+++ docs/projects/packing/packing.py
@@ -1,13 +1,28 @@
+import sys
 import Rhino.Geometry as rh
+import json
 
+### GET LOCAL PATH
+gh_path = ghenv.Component.OnPingDocument().FilePath.split("\\")
+print "Local GH path: {}".format(gh_path)
+
+#scripts_path = "\\".join(gh_path[:-1] + ["_scripts"])
+#if scripts_path not in sys.path: sys.path.append(scripts_path)
+
+import packing
+reload(packing)
+
+# need to add adjacencies in the initial fucntion---specified rooms
 
 class Agent:
 
-    def __init__(self, pt, r):
+    def __init__(self, pt, r,rooms, adjacenies):
 
         self.cp = pt
         self.radius = r
         self.neighbors = []
+        self.room = rooms
+        self.adjacency = adjacencies
 
     # method for adding another instance to a list of neighbors
     def add_neighbor(self, other):
@@ -91,20 +106,32 @@ class Agent:
         return rh.Circle(self.cp, self.radius)
 
 
-def run(pts, radii, max_iters, alpha, adjacencies):
+
+def run(pts, radii, rooms, adjacencies, max_iters, alpha):
 
     print(adjacencies)
 
     agents = []
 
     for i, pt in enumerate(pts):
-        my_agent = Agent(pt, radii[i])
+        my_agent = Agent(pt, radii[i], rooms[i], adjacencies[i])
+        
         agents.append(my_agent)
 
-    # for each agent in the list, add the previous agent as its neighbor
+    # for each agent in the list, add any listed agent as its neighbor
     for i in range(len(agents)):
-        agents[i].add_neighbor(agents[i-1])
+      
+       for j in range(len(agents)-1):
 
+            if agents[j].room in agents[i].adjacency:
+                # print(agents[j].name)
+                print(agents[i].adjacency)
+                agents[i].add_neighbor(agents[j])
+            else:
+                continue
+      
+      
+   
     for i in range(max_iters):
 
         total_amount = 0
@@ -113,12 +140,14 @@ def run(pts, radii, max_iters, alpha, adjacencies):
 
             # cluster to all agent's neighbors
             for agent_2 in agent_1.neighbors:
+                
+                
                 total_amount += agent_1.cluster(agent_2, alpha)
 
             # collide with all agents after agent in list
             for agent_2 in agents[j+1:]:
                 # add extra multiplier to decrease effect of cluster
-                total_amount += agent_1.collide(agent_2, alpha/5)
+                total_amount += agent_1.collide(agent_2, alpha/2)
 
         if total_amount < .01:
             break
@@ -133,3 +162,15 @@ def run(pts, radii, max_iters, alpha, adjacencies):
         circles.append(agent.get_circle())
 
     return circles, iters
+
+
+adjacencies = json.loads(adjacencies)
+
+print(adjacencies)
+
+circles, iters = packing.run(pts, radii, max_iters, alpha, adjacencies)
+
+
+
+
+
