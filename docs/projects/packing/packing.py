a7202d2d9190c77259a858523566195386c32a9f
ry2382 <114030175+ry2382@users.noreply.github.com>
2022-12-19 19:45:27 -0500

    Solving issue #89
    Boundary input takes a closed polyline. All circles will fit into the given irregular boundary shape.

--- docs/projects/packing/packing.py
+++ docs/projects/packing/packing.py
@@ -1,13 +1,19 @@
 import Rhino.Geometry as rh
 
+# set up geometry pipeline GH component for the boundary and add new varible to GH ptyhon
+# component with its type hint set to "polyline". draw a randown polyline as "boundary"(b)
+
 
 class Agent:
 
-    def __init__(self, pt, r):
+    # add boundry attriibute to agent
+
+    def __init__(self, pt, r, b):
 
         self.cp = pt
         self.radius = r
         self.neighbors = []
+        self.boundary = b
 
     # method for adding another instance to a list of neighbors
     def add_neighbor(self, other):
@@ -87,18 +93,99 @@ class Agent:
 
         return amount
 
+    # add new method for colliding with given boundary and clustering toward its center
+    def collideboundary(self, other, alpha):
+
+        # get overlapping distance
+        d = self.cp.DistanceTo(self.boundary.ClosestPoint(self.cp))
+
+        amount = 0
+
+        # check for overlapping
+        if d < self.radius:
+
+            pt_2 = self.boundary.CenterPoint()
+            pt_1 = self.cp
+
+            # get vector direction from self to boundary center point
+            v = pt_1 - pt_2
+
+            # change vector magnitude to 1
+            v.Unitize()
+            # set magnitude to half the overlap distance
+            v *= self.radius - d
+            # multiply by alpha parameter to control
+            # amount of movement at each time step
+            v *= alpha
+
+            amount = v.Length
+
+            # move other object
+            t = rh.Transform.Translation(v)
+            pt_2.Transform(t)
+
+            # reverse vector and move self same amount
+            # in opposite direction
+            v.Reverse()
+            t = rh.Transform.Translation(v)
+            pt_1.Transform(t)
+
+        return amount
+
+    # add new method for colliding with given boundary and clustering toward its center
+    def clusterboundary(self, other, alpha):
+
+        # get the distance between the agent and boundary center and the the its length that
+        # falls within the boundary
+        dCenterToSelf = self.cp.DistanceTo(self.boundary.CenterPoint())
+        dCenterToBoundary = self.boundary.CenterPoint().DistanceTo(
+            self.boundary.ClosestPoint(self.cp))
+
+        amount = 0
+
+        # compare and check if agent is outside the boundary
+        if dCenterToSelf > dCenterToBoundary:
+
+            pt_2 = self.boundary.CenterPoint()
+            pt_1 = self.cp
+
+            # get vector direction from self to boundary center point
+            v = pt_1 - pt_2
+
+            # change vector magnitude to 1
+            v.Unitize()
+            # set magnitude to half the overlap distance
+            v *= dCenterToSelf - dCenterToBoundary + 2 * self.radius
+            # multiply by alpha parameter to control
+            # amount of movement at each time step
+            v *= (alpha)
+
+            amount = v.Length
+
+            # move other object
+            t = rh.Transform.Translation(v)
+            pt_2.Transform(t)
+
+            # reverse vector and move self same amount
+            # in opposite direction
+            v.Reverse()
+            t = rh.Transform.Translation(v)
+            pt_1.Transform(t)
+
+        return amount
+
     def get_circle(self):
         return rh.Circle(self.cp, self.radius)
 
 
-def run(pts, radii, max_iters, alpha, adjacencies):
+def run(pts, radii, max_iters, alpha, adjacencies, b):
 
     print(adjacencies)
 
     agents = []
 
     for i, pt in enumerate(pts):
-        my_agent = Agent(pt, radii[i])
+        my_agent = Agent(pt, radii[i], b)
         agents.append(my_agent)
 
     # for each agent in the list, add the previous agent as its neighbor
@@ -119,6 +206,20 @@ def run(pts, radii, max_iters, alpha, adjacencies):
             for agent_2 in agents[j+1:]:
                 # add extra multiplier to decrease effect of cluster
                 total_amount += agent_1.collide(agent_2, alpha/5)
+
+        if total_amount < .01:
+            break
+
+        for j, agent_1 in enumerate(agents):
+
+            # all agent's neighbors cluster to boundary center point
+            for agent_2 in agent_1.neighbors:
+                total_amount += agent_1.clusterboundary(agent_2, alpha)
+
+            # collide with boundary after agent in list
+            for agent_2 in agents[j+1:]:
+                # add extra multiplier to decrease effect of cluster
+                total_amount += agent_1.collideboundary(agent_2, alpha/5)
 
         if total_amount < .01:
             break
